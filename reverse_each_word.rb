97c558b1f3de6eaabc970559e8fb17387284e9e7
Daksha Patel <dakshapatel100@gmail.com>
2018-05-03 21:15:15 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,11 +8,3 @@ def reverse_each_word(string)
   end
   sentence
 end
-
-
-def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
-  word.reverse
-end
-  reversed.join(" ")
-end 
