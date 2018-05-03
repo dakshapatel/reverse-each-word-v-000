da32a0df2bd5dc0d657f5af63afabeea1386a4b9
Daksha Patel <dakshapatel100@gmail.com>
2018-05-03 21:16:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 string= "Hello there, and how are you?"
 
-def reverse_each_word(string)
-  string.split(" ")
-  sentence = []
-  string.each do |line|
-    sentence.reverse
-  end
-  sentence
+
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
 end
